27e3872b445153360edd1803f65f0afe0698d5eb
aggolb <aggolb@gmail.com>
2017-04-10 16:29:27 +0200

    added mango database

--- mango/mango/settings.py
+++ mango/mango/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mango',
+        'USER': 'postgres',
+        'PASSWORD': 'family',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
