09e9ef30af55c68100100d3edd4a3e3cf0499c28
aggolb <aggolb@gmail.com>
2017-04-16 15:51:38 +0200

    added read field to message model

--- mango/message_system/models.py
+++ mango/message_system/models.py
@@ -9,6 +9,7 @@ class Message(models.Model):
     subject = models.CharField(max_length = 200, null = True, blank = True)
     date_sent = models.DateTimeField(auto_now_add = True)
     body = models.TextField(null=True, blank=True)
+    read = models.BooleanField(default = False)
 
     def __unicode__(self):
         return "From: %s to %s - Subject: %s - Date: %s"%(self.sender,self.receiver,self.subject,self.date_sent)
